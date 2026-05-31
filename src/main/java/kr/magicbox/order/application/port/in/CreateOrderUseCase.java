5550c11d2612c574e5d5a7abe66ded8894bbee2d
Lian08 <lian080408@gmail.com>
2026-06-01 08:42:30 +0900

    feat/147 :: CreateOrderUseCase, CreateReleaseOrderUseCase 반환 타입 CreateOrderResult로 변경

--- src/main/java/kr/magicbox/order/application/port/in/CreateOrderUseCase.java
+++ src/main/java/kr/magicbox/order/application/port/in/CreateOrderUseCase.java
@@ -1,7 +1,8 @@
 package kr.magicbox.order.application.port.in;
 
 import kr.magicbox.order.application.dto.command.CreateOrderCommand;
+import kr.magicbox.order.application.dto.result.CreateOrderResult;
 
 public interface CreateOrderUseCase {
-    void createOrder(CreateOrderCommand command);
+    CreateOrderResult createOrder(CreateOrderCommand command);
 }
